88a212a188c3ec2b9d35339b72ef48c2225301f3
vonacht <vonachtf80@gmail.com>
2026-01-15 00:26:03 +0100

    removed need for ENV var

--- uassetgen.py
+++ uassetgen.py
@@ -4,26 +4,22 @@ import logging
 load("coreclr")
 import clr
 import json
-import os
+from pathlib import Path
 
 
 def JSON_to_uasset(
     input_json: dict, output_path: str = "cache/RMG_ExtractionLinear_patched.uasset"
 ):
-    dll_path = os.environ["UASSETAPI_DLL_PATH"]
-    # Load the assembly:
-    clr.AddReference(f"{dll_path}/libs/UAssetAPI.dll")
+    # Load the assembly. The dll_path needs to be an absolute reference to libs/UAssetAPI.dll:
+    dll_path = Path.cwd() / "libs" / "UAssetAPI.dll"
+    clr.AddReference(str(dll_path))
     # We load the methods to read from JSON and save:
     from UAssetAPI import UAsset
     from UAssetAPI import UnrealTypes
 
     # DeserializeJson expects a string:
     UAsset.DeserializeJson(json.dumps(input_json)).Write(
-        f"cache/RMG_ExtractionLinear_patched_pre.uasset"
+            output_path
     )
-    json_string = UAsset(
-        f"cache/RMG_ExtractionLinear_patched_pre.uasset",
-        UnrealTypes.EngineVersion.VER_UE4_27,
-    ).SerializeJson()
-    UAsset.DeserializeJson(json_string).Write(output_path)
     logging.info(f"Writing the patched UAsset in {output_path}")
+    
