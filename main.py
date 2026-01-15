4b0f881e1093d47954beeb8dc698e1e2b65d50a3
vonacht <vonachtf80@gmail.com>
2026-01-15 01:06:24 +0100

    added debug flag, added support for removing rooms

--- main.py
+++ main.py
@@ -11,11 +11,17 @@ logging.basicConfig(
     level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s"
 )
 
+def find_import_idx(import_list, room):
+    for ii, obj in enumerate(import_list):
+        if obj["ObjectName"] == room:
+            return -(ii+1)
+    return None
+
 
 def main():
 
     parser = argparse.ArgumentParser(
-        description="Example CLI with a positional argument"
+        description="Patcher for DRG's RMG_ExtractionLinear file. Used to add custom rooms or remove existing rooms."
     )
     parser.add_argument(
         "-c",
@@ -29,7 +35,13 @@ def main():
         "--output_path",
         nargs="?",
         default="cache/RMG_ExtractionLinear_patched.uasset",
-        help="Path where the patched RMG file will be written.",
+        help="Path where the patched RMG file will be written. If not specified defaults to cache/",
+    )
+    parser.add_argument(
+        "-d",
+        "--debug",
+        action="store_true",
+        help="Writes the patched UAsset in JSON form together with the output in the cache/ directory.",
     )
     args = parser.parse_args()
 
@@ -53,40 +65,63 @@ def main():
     ## TODO: add jsonschema validation
 
     patched_json = copy.deepcopy(original_rmg)
-    for ii, room_to_add in enumerate(user_config_file["Add"]):
-        # 1. Patch the imports:
-        room_path = DEFAULT_ROOMS_PATH + room_to_add
-        package_import = copy.deepcopy(default_import)
-        package_import.update(
-            {"ObjectName": room_path, "OuterIndex": 0, "ClassName": "Package"}
-        )
-        patched_json["Imports"].append(package_import)
-        room_import = copy.deepcopy(default_import)
-        room_import.update(
-            {
-                "ObjectName": room_to_add,
-                "OuterIndex": -(import_length + 1 + 2 * ii),
-                "ClassName": "RoomGenerator",
-                "ClassPackage": "/Script/FSD"
-            }
-        )
-        patched_json["Imports"].append(room_import)
-        # 2. Patch the exports:
-        room_export = copy.deepcopy(default_export_entry)
-        room_export.update(
-            {"Name": f"{ii + export_length}", "Value": -(import_length + 2 + 2 * ii)}
-        )
-        patched_json["Exports"][0]["Data"][0]["Value"].append(room_export)
-        patched_json["Exports"][0]["CreateBeforeSerializationDependencies"].append(-(import_length + 2 + 2*ii))
-        # 3. Patch the NameMap:
-        patched_json["NameMap"].extend([room_path, room_to_add])
-    with open("assets/test.json", "w") as f:
-        json.dump(patched_json, f, indent=4)
-    try:
-        JSON_to_uasset(patched_json, args.output_path)
-    except Exception as e:
-        logging.error(f"Error when writing the patched UAsset: {e}")
+    if user_config_file["Add"]:
+        for ii, room_to_add in enumerate(user_config_file["Add"]):
+            # 1. Patch the imports:
+            room_path = DEFAULT_ROOMS_PATH + room_to_add
+            package_import = copy.deepcopy(default_import)
+            package_import.update(
+                {"ObjectName": room_path, "OuterIndex": 0, "ClassName": "Package"}
+            )
+            patched_json["Imports"].append(package_import)
+            room_import = copy.deepcopy(default_import)
+            room_import.update(
+                {
+                    "ObjectName": room_to_add,
+                    "OuterIndex": -(import_length + 1 + 2 * ii),
+                    "ClassName": "RoomGenerator",
+                    "ClassPackage": "/Script/FSD"
+                }
+            )
+            patched_json["Imports"].append(room_import)
+            # 2. Patch the exports:
+            room_export = copy.deepcopy(default_export_entry)
+            room_export.update(
+                {"Name": f"{ii + export_length}", "Value": -(import_length + 2 + 2 * ii)}
+            )
+            patched_json["Exports"][0]["Data"][0]["Value"].append(room_export)
+            patched_json["Exports"][0]["CreateBeforeSerializationDependencies"].append(-(import_length + 2 + 2*ii))
+            # 3. Patch the NameMap:
+            patched_json["NameMap"].extend([room_path, room_to_add])
+            logging.info(f"Added room {room_to_add}.")
+    else:
+        logging.info("No room to add found in config file. Skipping.")
 
+    if user_config_file["Remove"]:
+        for room_to_remove in user_config_file["Remove"]:
+            room_idx = find_import_idx(original_rmg["Imports"], room_to_remove)
+            if room_idx is None:
+                logging.info(f"Room to remove {room_to_remove} no found in file. Skipping.")
+                continue
+            else:
+                exports = copy.deepcopy(patched_json["Exports"])
+                exports[0]["Data"][0]["Value"] = [exp for exp in exports[0]["Data"][0]["Value"] if exp["Value"] != room_idx]
+                patched_json["Exports"] = exports
+                logging.info(f"Removed room {room_to_remove}.")
+    else:
+        logging.info("No room to remove found in config file. Skipping.")
+
+    if user_config_file["Add"] or user_config_file["Remove"]:
+        if args.debug:
+            with open("cache/json_debug.json", "w") as f:
+                json.dump(patched_json, f, indent=4)
+            logging.info("Writing debug JSON in cache/json_debug.json")
+        try:
+            JSON_to_uasset(patched_json, args.output_path)
+        except Exception as e:
+            logging.error(f"Error when writing the patched UAsset: {e}")
+    else:
+        logging.info("Config file empty. Finishing.")
 
 if __name__ == "__main__":
     main()
